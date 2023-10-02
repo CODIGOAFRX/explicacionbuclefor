21047b35895aef20965d26231b871c601e6ee056
CODIGOAFRX <145802023+CODIGOAFRX@users.noreply.github.com>
2023-10-02 17:52:45 +0200

    Add files via upload

---
+++ Bucleefor.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bucleefor;
+
+/**
+ *
+ * @author AlumnoT
+ */
+public class Bucleefor {
+
+    public static void main(String[] args) {
+       for(int i=0; i<10;i++){                                    // i++ significa que a i se le va sumando 1
+           System.out.println("iteracion " +i );         // sout + tab para acortar
+       }
+    }
+}
